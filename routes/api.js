d2a36a37f5f37c786b4dd97e843286f06a3f580a
Mark Adkins <mark@bymark.co>
2020-02-29 17:40:45 -0500

    Example Update

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const { execSync } = require('child_process');
+const { exec } = require('child_process');
+let path = require('path');
 
 router.get('/', function(req, res, next) {
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify({status:"it works!"}));
+});
+
+router.get('/test', function(req, res, next) {
+  res.setHeader('Content-Type', 'application/json');
+  let output = "";
+  exec(path.resolve(__dirname, '..') + '/scripts/test.sh', (err, stdout, stderr) => {
+    if (err) {
+      output = err;
+    } else {
+      output = stdout;
+    }
+    res.send(JSON.stringify({status: output}));
+  });
 });
 
 router.get('*', function(req, res, next) {
